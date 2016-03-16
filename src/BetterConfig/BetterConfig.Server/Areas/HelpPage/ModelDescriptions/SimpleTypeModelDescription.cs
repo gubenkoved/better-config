c4927ff659bf26de408271318c243944073bc92f
Eugene Gubenkov <gubenkoved@gmail.com>
2016-03-17 00:23:42 +0300

    Added Server and Server Test projects.

---
+++ src/BetterConfig/BetterConfig.Server/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BetterConfig.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ src/BetterConfig/BetterConfig.Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BetterConfig.Server.WebApiApplication" Language="C#" %>
